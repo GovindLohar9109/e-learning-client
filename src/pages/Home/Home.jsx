0aab308c178b8c7b4fc5e9806b19694c118c0f63
GovindLohar9109 <govind@gkmit.co>
2025-11-14 14:26:13 +0530

    style : formatting ,indentation

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import Background from "../../components/common/Background.jsx";
+export default function Home() {
+    return <>
+        <Background img={"bg1"} />
+        <h1>Courses will be there</h1>
+       </>
+}
