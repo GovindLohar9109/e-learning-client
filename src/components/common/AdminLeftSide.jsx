cbb636a7b83062ecaf78bc5803e5ea2be8ac7e06
GovindLohar9109 <govind@gkmit.co>
2025-11-14 17:51:11 +0530

    chore: add AdminLeftSide.jsx

---
+++ src/components/common/AdminLeftSide.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router";
+
+export default function AdminLeftSide() {
+return (<div className="bg-secondary p-3  h-100" style={{  maxWidth: "300px" }}>
+                <div className="m-2">
+                    <div className="w-100  m-auto " style={{ height: "250px" }}  >
+                        <img style={{ borderRadius: "100%" }} src={`${process.env.PUBLIC_URL}/img/bg1.png`} className="w-100 h-75 m-auto d-block" alt="" />
+                    </div>
+                    <div className="mt-5">
+                        <ul className="list-group d-flex flex-dir-col align-items-space-between gap-3">
+                            <NavLink to="/admin"className="list-group-item bg-primary">Admin DashBoard</NavLink>
+                            <NavLink to="courses-manage"className="list-group-item bg-primary">Courses Management</NavLink>
+                           
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+}
