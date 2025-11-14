22d9d369829e4cda38635a0b53f50f1dafc21f18
GovindLohar9109 <govind@gkmit.co>
2025-11-15 01:05:52 +0530

    chore: updated CourseCard file

--- src/components/cards/CourseCard.jsx
+++ src/components/cards/CourseCard.jsx
@@ -1,23 +1,22 @@
 import { NavLink } from "react-router";
 
 export default function CourseCard({ course }) {
-
     function handleAddToMyCourse() {
-        // add to my card logic will be there
+        
     }
     return (
         <div className="card" style={{ width: "25rem" }}>
             <div className="">
-                <img src={`${process.env.PUBLIC_URL}/img/${"CSFunda/cn.png"}`} className="w-100 h-100 card-img-top" alt="..." />
+                <img src={`${process.env.PUBLIC_URL}/img/${course.image_url}`} className="w-100 h-100 card-img-top" alt="..." />
             </div>
             <div className="card-body">
-                <h5 className="card-title text-bold"></h5>
-                <p className="card-text"></p>
-                <span>Duration : 3 Hour</span>
+                <h5 className="card-title text-bold">{course.name}</h5>
+                <p className="card-text">{course.description}</p>
+                <span>Duration : {course.duration}</span>
             </div>
             <ul className="list-group list-group-flush text-center align-items-center" >
-                <li className="list-group-item w-100"><NavLink to="" className="text-deco-none"><button type="button" style={{ width: "100%" }} className="btn btn-danger  d-block text-bold" >Documentation</button></NavLink></li>
-                <li className="list-group-item w-100"> <NavLink to="" className="text-deco-none"><button type="button" style={{ width: "100%" }} className="btn btn-secondary d-block text-bold">Details </button> </NavLink></li>
+                <li className="list-group-item w-100"><NavLink to={course.link} className="text-deco-none"><button type="button" style={{ width: "100%" }} className="btn btn-danger  d-block text-bold" >Documentation</button></NavLink></li>
+                <li className="list-group-item w-100"> <NavLink to="/course-details/1" className="text-deco-none"><button type="button" style={{ width: "100%" }} className="btn btn-secondary d-block text-bold">Details </button> </NavLink></li>
                 <li className="list-group-item w-100"><button type="button" onClick={handleAddToMyCourse} style={{ width: "100%" }} className="btn btn-primary d-block text-bold">Add To My</button></li>
             </ul>
         </div>
