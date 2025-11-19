a17e5888b49b4f219b9d74bcbf5ca5d077bb8934
GovindLohar9109 <govind@gkmit.co>
2025-11-19 13:38:39 +0530

    resolve conflicts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import Background from "../../components/common/Background.jsx";
 import Courses from "../course/Courses.jsx";
 
 export default function Home() {
-
     return (<>
         <Background img={"bg1"} />
         <Courses />
--- src/pages/course/AllCourses.jsx
+++ src/pages/course/AllCourses.jsx
@@ -2,10 +2,7 @@ import Background from "../../components/common/Background"
 import CourseCard from "../../components/cards/CourseCard"
 
 export default function AllCourses() {
-
-
-    return <>
-        <div className="p-5">
+    return <div className="p-5">
             <div>
                 <Background img={"bg2"} />
             </div>
@@ -23,5 +20,4 @@ export default function AllCourses() {
                 </div>
             </div>
         </div>
-    </>
-}
+    }
