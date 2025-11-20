95369f828999ff12546ff9f2771f850a6d308271
GovindLohar9109 <govind@gkmit.co>
2025-11-20 10:20:20 +0530

    remove inline styling

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -22,8 +22,8 @@ export default function Register() {
         setUser({ ...user, [name]: value });
     }
     return (
-        <div className="d-flex align-items-center w-100" style={{ height: "80vh" }}>
-            <div className="container p-4  shadow rounded " style={{ maxWidth: "500px" }}>
+        <div className="d-flex align-items-center w-100 register" >
+            <div className="container p-4  shadow rounded " >
                 {(message.status) ?
                     <div class="alert alert-primary" role="alert">
                         {message.msg}
